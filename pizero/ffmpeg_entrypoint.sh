77a49b46c7728aa1de00528c4fe42de9051e0b2d
Tyler Moore <TylerRMoore@gmail.com>
2021-08-06 09:21:36 -0700

    Trying hardware encoding

--- pizero/ffmpeg_entrypoint.sh
+++ pizero/ffmpeg_entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ffmpeg -f v4l2 -i /dev/video0 -f rtsp rtsp://localhost:8554/video0
+#ffmpeg -f v4l2 -i /dev/video0 -f rtsp rtsp://localhost:8554/video0
+ffmpeg -f v4l2 -i /dev/video0 -c:v h264_omx -f rtsp rtsp://localhost:8554/video0 
+#ffmpeg -f v4l2 -i /dev/video0 -c:v h264_v4l2m2m -f rtsp rtsp://localhost:8554/video0 
