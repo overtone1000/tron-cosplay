e48abdfe88c8fcefcf84eaa3c45cede352e9c08a
Tyler Moore <TylerRMoore@gmail.com>
2021-08-08 03:13:28 -0700

    Working stream, but motion doesn't recognize it.

--- pizero/ffmpeg_entrypoint.sh
+++ pizero/ffmpeg_entrypoint.sh
@@ -6,13 +6,7 @@
 #get output sizes with v4l2-ctl -i /dev/video0 --list-framesizes FORMAT_TYPE_FROM_LISTFORMATS
 
 #ffmpeg -f v4l2 -i /dev/video0 -f rtsp rtsp://localhost:8554/video0
-ffmpeg -f v4l2 -i /dev/video0 -c:v h264_omx -f rtsp rtsp://localhost:8554/video0 
-#ffmpeg -f v4l2 -i /dev/video0 -c:v h264_v4l2m2m -f rtsp rtsp://localhost:8554/video0
-#ffmpeg -f mjpeg -s 1920x1080 -i /dev/video0 -f rtsp rtsp://localhost:8554/video0
+#ffmpeg -f v4l2 -i /dev/video0 -c:v h264_omx -f rtsp rtsp://localhost:8554/video0 
 
-#This is close, but still converts it to mp4!!
-#ffmpeg -video_size 1280x1024 -input_format mjpeg -i /dev/video0 -f rtsp -c mjpeg rtsp://localhost:8554/video0
-
-#Gives error about chroma blocks...
-#ffmpeg -video_size 1280x1024 -input_format mjpeg -i /dev/video0 -f rtsp -c mjpeg rtsp://localhost:8554/video0
-#Probably need to change pixel format.
+#This works!
+ffmpeg -video_size 1280x960 -input_format mjpeg -i /dev/video0 -vcodec copy -f rtsp rtsp://localhost:8554/video0
