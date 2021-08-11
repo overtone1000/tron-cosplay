eb1f98e650e70d04a10c9de6b1c5cc479c8c7c29
Tyler Moore <tylerrmoore@gmail.com>
2021-08-11 11:28:54 -0700

    Update ffmpeg_entrypoint.sh

--- pizero/ffmpeg_entrypoint.sh
+++ pizero/ffmpeg_entrypoint.sh
@@ -8,8 +8,11 @@
 #ffmpeg -f v4l2 -i /dev/video0 -f rtsp rtsp://localhost:8554/video0
 #ffmpeg -f v4l2 -i /dev/video0 -c:v h264_omx -f rtsp rtsp://localhost:8554/video0
 
-#This works! But motion can't read the stream for some reason...
+#This works!
 #ffmpeg -video_size 1280x960 -input_format mjpeg -i /dev/video0 -vcodec copy -f rtsp rtsp://localhost:8554/video0
+#Works with the latest version of motion with the netcam_decoder config set to "mjpeg"
+#However, while the mjpeg stream is very fast (30 fps), it isn't reliably readable by VLC or by a browser using the HLS stream from simple_rstp_server. 
+#This is a wash.
 
-#This works! But only 5 fps. Not really hardware accelerated?
+#This works! But only 5 fps. Not really hardware accelerated? Probably just limitation of the pizero. Oh well.
 ffmpeg -video_size 1280x960 -i /dev/video0 -c:v h264_omx -pix_fmt yuv420p -f rtsp rtsp://localhost:8554/video0
